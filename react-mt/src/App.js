9701bfaeb1bb72544bf0c6d545906a369a6be9fe
Ali Azizi <aliazizy@hotmail.com>
2020-06-22 16:12:14 +0100

    test 2

--- react-mt/src/App.js
+++ react-mt/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hello world</p>
+        <p>hello world yokh baba</p>
         <a
           className="App-link"
           href="https://reactjs.org"
